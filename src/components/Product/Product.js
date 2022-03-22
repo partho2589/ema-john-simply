8771fd55bd724b49c774259c9da54313f4974541
partho2589 <parthomalo615@.com>
2022-03-22 11:57:28 +0600

    added product detials

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,7 +6,16 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
+            <div className='product-details'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Manufacture: {seller}</small></p>
+                <p><small>Rating: {ratings} </small></p>
+                
+            </div>
+            <button className='btn-card'>
+                <p>Add to card</p>
+            </button>
         </div>
     );
 };
